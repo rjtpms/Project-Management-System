7d19e57ac08def2072ac07671996b5c44b1321ab
Chico Lin <soap.cmu@gmail.com>
2018-01-22 20:19:07 -0600

    start project

---
+++ Project2-PMS/Project2-PMS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project2-PMS
+//
+//  Created by LinChico on 1/22/18.
+//  Copyright © 2018 RJTCompuquest. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
