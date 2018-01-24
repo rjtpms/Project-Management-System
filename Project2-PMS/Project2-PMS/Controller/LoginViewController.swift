65a6fa2a5971f9ed47ed349703d6518174eb3be2
YingzhengMa <yma004@drury.edu>
2018-01-24 01:00:52 -0600

    finished Login View Controller and screen (Email/password login and Google OAuth); Refactor code

--- Project2-PMS/Project2-PMS/Controller/LoginViewController.swift
+++ Project2-PMS/Project2-PMS/Controller/LoginViewController.swift
@@ -7,10 +7,162 @@
 //
 
 import UIKit
+import TWMessageBarManager
+import GoogleSignIn
+import FirebaseAuth
 
 class LoginViewController: UIViewController {
-
+	@IBOutlet weak var loginButton: UIButton!
+	@IBOutlet weak var emailTextfield: UITextField!
+	@IBOutlet weak var passwordTextfield: UITextField!
+	
+	lazy var tapRecognizer: UITapGestureRecognizer = {
+		var recognizer = UITapGestureRecognizer(target:self, action: #selector(dismissKeyboard))
+		return recognizer
+	}()
+	
+	private var userEmail: String!
+	private var userPassword: String!
+	
+	// MARK: - Viewcontroller Lifecycles
     override func viewDidLoad() {
         super.viewDidLoad()
+		setupUI()
     }
+	
+	override func viewDidAppear(_ animated: Bool) {
+		super.viewDidAppear(animated)
+		view.addGestureRecognizer(tapRecognizer)
+	}
+	
+	override func viewWillDisappear(_ animated: Bool) {
+		super.viewWillDisappear(animated)
+		dismissKeyboard()
+		view.removeGestureRecognizer(tapRecognizer)
+	}
+	
+	// MARK: - IBActions
+	@IBAction func forgotPassword(_ sender: UIButton) {
+		// TODO: Reset password Page
+	}
+	
+	@IBAction func login(_ sender: UIButton) {
+		loginUser()
+	}
+	
+	@IBAction func googleLogin(_ sender: UIButton) {
+		GIDSignIn.sharedInstance().uiDelegate = self
+		GIDSignIn.sharedInstance().delegate = self
+		GIDSignIn.sharedInstance().signIn()
+	}
+}
+
+// MARK: - Helper Methods
+private extension LoginViewController {
+	func setupUI() {
+		loginButton.layer.cornerRadius = 5
+		loginButton.layer.borderWidth = 1
+		loginButton.layer.borderColor = UIColor.gray.cgColor
+		loginButton.clipsToBounds = true
+	}
+	
+	@objc func dismissKeyboard() {
+		view.endEditing(true)
+	}
+	
+	func validateInputs() -> Bool {
+		// validate
+		guard let email = emailTextfield.text?.trimmingCharacters(in: .whitespacesAndNewlines),
+			!email.isEmpty else {
+			TWMessageBarManager().showMessage(withTitle: "Error", description: "Email can not be empty", type: .error)
+			return false
+		}
+		
+		guard let password = passwordTextfield.text?.trimmingCharacters(in: .whitespacesAndNewlines),
+			!password.isEmpty else {
+			TWMessageBarManager().showMessage(withTitle: "Error", description: "Password can not be empty", type: .error)
+			return false
+		}
+		
+		userEmail = email
+		userPassword = password
+		
+		// Clear inputs once validated 
+		emailTextfield.text = ""
+		passwordTextfield.text = ""
+		
+		return true
+	}
+	
+	func loginUser(_ credential: AuthCredential? = nil) {
+		// handle oAuth signin
+		if let cred = credential {
+			FIRService.shareInstance.loginUser(with: cred) { [weak self ] (user, error) in
+				self?.proceedLogin(for: user, with: error)
+			}
+		} else {
+			// Handle Email/Password login
+			if validateInputs() {
+				
+				// Call FIRService to login user with email and password
+				FIRService.shareInstance.loginUser(with: userEmail, and: userPassword) { [weak self] (user, error) in
+					self?.proceedLogin(for: user, with: error)
+				}
+			}
+		}
+	}
+	
+	func proceedLogin(for user: User?, with error: Error?) {
+		guard error == nil, let user = user else {
+			TWMessageBarManager().showMessage(withTitle: "Error", description: error!.localizedDescription, type: .error)
+			return
+		}
+		
+		print(user.displayName ?? "")
+		print(user.uid)
+		print(user.email ?? "")
+		print(user.photoURL?.absoluteString ?? "")
+		
+		// Save user info in firebase, and in sinleton
+		FIRService.shareInstance.saveLoggedInUser(user) {
+			// Navigate to homeVC and we are on main queue
+		}
+	}
+}
+
+extension LoginViewController: GIDSignInUIDelegate, GIDSignInDelegate {
+	func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
+		
+		if let error = error {
+			TWMessageBarManager().showMessage(withTitle: "Error", description: error.localizedDescription, type: .error)
+			return
+		}
+		
+		guard let authentication = user.authentication else  {
+			return
+		}
+		
+		let credential = GoogleAuthProvider.credential(withIDToken: authentication.idToken, accessToken: authentication.accessToken)
+		
+		print("Got credential, ready to login")
+		loginUser(credential)
+	}
+}
+
+extension LoginViewController: UITextFieldDelegate {
+	func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+		switch textField {
+		case emailTextfield:
+			passwordTextfield.becomeFirstResponder()
+		case passwordTextfield:
+			passwordTextfield.resignFirstResponder()
+			
+			// Login user when press "GO"
+			loginUser()
+		default:
+			break
+		}
+		
+		return true
+	}
 }
--- Project2-PMS/Project2-PMS/Network/FIRService.swift
+++ Project2-PMS/Project2-PMS/Network/FIRService.swift
@@ -7,15 +7,97 @@
 //
 
 import Foundation
+import FirebaseAuth
+import FirebaseDatabase
 
 enum Result<T> {
 	case Success(T)
 	case Error(String)
 }
 
+typealias LoginResultHandler = (User?, Error?) -> ()
+
 class FIRService: NSObject {
 	static let shareInstance = FIRService()
-	private override init() {}
 	
+	// Database refences
+	private var userRef: DatabaseReference!
 	
+	private override init() {
+		userRef = Database.database().reference().child("Users")
+	}
+	
+	// Email,Password login
+	func loginUser(with email: String, and password: String, completion: @escaping LoginResultHandler) {
+		Auth.auth().signIn(withEmail: email, password: password) { (user, error) in
+			DispatchQueue.main.async {
+				completion(user, error)
+			}
+		}
+	}
+	
+	// OAuth Login
+	func loginUser(with credential: AuthCredential, completion: @escaping LoginResultHandler) {
+		Auth.auth().signIn(with: credential) { (user, error) in
+			DispatchQueue.main.async {
+				completion(user, error)
+			}
+		}
+	}
+	
+	// Save loggedin user info in Firebase Users table and store into as CurrentUser singleton
+	func saveLoggedInUser(_ user: User, completion: @escaping () -> ()) {
+		let currentUser = CurrentUser.sharedInstance
+		
+		userRef.observeSingleEvent(of: .value) { [weak self] (snapshot) in
+			if !snapshot.hasChild(user.uid) {
+				// If record not exist, create one (only for OAuth)
+				self?.userRef.child(user.uid).updateChildValues(
+					["name": user.displayName ?? "",
+					 "email": user.email!,
+					 "profile photo": user.photoURL?.absoluteString ?? ""
+					]
+				)
+				// save oAuth user info to current user
+				currentUser.userId = user.uid
+				currentUser.email = user.email
+				currentUser.fullname = user.displayName
+				currentUser.profileImageUrl = user.photoURL
+				
+				DispatchQueue.main.async {
+					completion()
+				}
+			} else {
+				// if record exist, fetch it
+				self?.fetchCurrentUserInfo(with: user.uid) {
+					DispatchQueue.main.async {
+						completion()
+					}
+				}
+			}
+		}
+	}
+	
+	// Fetch currentuser info from firebase and store in singleton
+	func fetchCurrentUserInfo(with id: String, completion: @escaping () -> ()) {
+		let currentUser = CurrentUser.sharedInstance
+		
+		userRef.child(id).observeSingleEvent(of: .value) { (snapshot) in
+			if let userDict = snapshot.value as? [String: String] {
+				let email = userDict["email"]!
+				let name = userDict["name"]
+				let profileUrlStr = userDict["profile photo"]
+				
+				if let profilestr = profileUrlStr {
+					currentUser.profileImageUrl = URL(string: profilestr)
+				}
+				currentUser.email = email
+				currentUser.fullname = name
+				
+				DispatchQueue.main.async {
+					completion()
+				}
+			}
+		}
+	}
 }
