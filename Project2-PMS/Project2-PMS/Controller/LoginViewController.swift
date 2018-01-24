ce7a7b679b26f0d783565fb94ce8fa5bb97ae657
YingzhengMa <yma004@drury.edu>
2018-01-23 20:17:49 -0600

    configure Firebase and initialize pod

---
+++ Project2-PMS/Project2-PMS/Controller/LoginViewController.swift
@@ -0,0 +1,16 @@
+//
+//  LoginViewController.swift
+//  Project2-PMS
+//
+//  Created by Mark on 1/23/18.
+//  Copyright © 2018 RJTCompuquest. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
